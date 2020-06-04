da5b45e6dabfe10ca7ab44185ba2b6723a931ef3
Brian Rohan <brohan@outlook.com>
2020-06-04 16:24:16 +0200

    Add DealingWithErrorsAndFaultsService

--- BrianClient/StreamExtensions.cs
+++ BrianClient/StreamExtensions.cs
@@ -7,6 +7,30 @@ namespace BrianClient
 {
     public static class StreamExtensions
     {
+
+
+        public static object ReadAndDeSerializeFromJson(this Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (!stream.CanRead)
+            {
+                throw new NotSupportedException("Can't read from this stream.");
+            }
+
+            using (var streamReader = new StreamReader(stream))
+            {
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    return jsonSerializer.Deserialize(jsonTextReader);
+                }
+            }
+        }
+
         public static T ReadAndDeserializeFromJson<T>(this Stream stream)
         {
             if (stream == null)
